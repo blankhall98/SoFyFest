84479a32bbec160f4fc27d71ceb0db6afc65e8cd
blankhall98 <blankhall@ciencias.unam.mx>
2022-04-07 15:12:15 -0500

    changes sqlite for postresql

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "party_app"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://djwskmmwwxhmtp:3c4cde0c42c5f80f868741907f5e620840bb14b92e7902c9fa35d114c68aa54c@ec2-3-224-125-117.compute-1.amazonaws.com:5432/dftmfhh9985v2a'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -36,7 +36,7 @@ def main():
         db.session.add(guest)
         db.session.commit()
 
-        return render_template('home.html',guests = users.query.all())
+        return render_template('home.html',guests = users.query.all(),message="¡Te registraste Exitosamente!")
     else:
         return render_template('home.html',guests = users.query.all())
 
